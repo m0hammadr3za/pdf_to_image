1b67ab8f811b70fc74c63e1be6fe6c7118e719e3
m0hammadr3za <mohammad.reza.gholami.dev@gmail.com>
2022-01-09 04:45:41 +0330

    Refactored convert script

--- script.py
+++ script.py
@@ -1,37 +1,64 @@
 import os
+from pathlib import Path
 from pdf2image import convert_from_path
 
-import_directory = "pdf"
-export_directory = "image"
+import_path = Path("./pdfs")
+export_path = Path(import_path, "../images")
 
-def pdf_to_image(import_directory, export_directory):
-    pdf_file_names = get_file_names_from_pdf_files(import_directory)
+def convert_directory_pdfs_to_images():
+    print("started converting from...\n")
 
-    for file_name in pdf_file_names:
-        pdf_path = "./" + import_directory + "/" + file_name + ".pdf"
-        images = convert_from_path(pdf_path)
+    validate_import_path(import_path)
+    create_export_directory(export_path)
 
-        images_directory = "./" + export_directory + "/" + file_name
-        if not(os.path.exists(images_directory)): os.mkdir(images_directory)
-        else: clear_directory(images_directory)
+    pdf_file_names = get_directory_pdf_file_names(import_path)
+    for pdf_file_name in pdf_file_names:
+        pdf_file_path = Path(import_path, pdf_file_name + ".pdf")
+        convert_pdf_to_images(pdf_file_path, pdf_file_name, export_path)
+    
+    print("finished converting.")
 
-        image_counter = 1
-        for image in images:
-            image_path = images_directory + "/" + str(image_counter) + ".jpg"
-            image.save(image_path, "JPEG")
-            image_counter += 1
+def validate_import_path(import_path):
+    if not(os.path.exists(import_path)):
+        return print("\"" + import_path + "\"" + " path does not exist!")
 
-def get_file_names_from_pdf_files(directory):
+def create_export_directory(export_path):
+    if not(os.path.exists(export_path)):
+        os.mkdir(export_path)
+
+def get_directory_pdf_file_names(directory_path):
     file_names = []
 
-    for file_name in os.listdir(directory):
+    for file_name in os.listdir(directory_path):
         pdf_name, pdf_extention = os.path.splitext(file_name)
         if pdf_extention == ".pdf": file_names.append(pdf_name)
     
     return file_names
 
-def clear_directory(directory):
-    for file in os.listdir(directory):
-        os.remove(os.path.join(directory, file))
+def convert_pdf_to_images(pdf_file_path, pdf_file_name, export_path):
+    print("started converting \"" + pdf_file_name + ".pdf\"")
 
-pdf_to_image(import_directory, export_directory)
+    images = convert_from_path(pdf_file_path)
+    images_export_path = Path(export_path, pdf_file_name)
+
+    create_export_directory(images_export_path)
+    clear_directory(images_export_path)
+
+    save_images(images, images_export_path)
+
+    print("finished converting \"" + pdf_file_name + ".pdf\"\n")
+    
+def clear_directory(images_export_path):
+    for file in os.listdir(images_export_path):
+        os.remove(os.path.join(images_export_path, file))
+
+def save_images(images, images_export_path):
+    image_counter = 1
+    for image in images:
+        image_full_name = str(image_counter) + ".jpg"
+        image_path = Path(images_export_path, image_full_name)
+        image.save(image_path, "JPEG")
+        image_counter += 1
+
+convert_directory_pdfs_to_images()
+input("Press any key to exit.")
